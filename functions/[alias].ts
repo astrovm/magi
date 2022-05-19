bbd3ef965478aee2f9bedf1cd12480a97585894c
astrolince <astro@astrolince.com>
2022-05-19 20:56:25 -0300

    add cache and size checks

--- functions/[alias].ts
+++ functions/[alias].ts
@@ -2,7 +2,7 @@ export async function onRequestGet({ env, params, request }) {
     if (params.alias.includes('.')) {
         return env.ASSETS.fetch(request);
     }
-    const destinationURL = await env.links.get(params.alias);
+    const destinationURL = await env.links.get(params.alias, { cacheTtl: 86400 });
     const statusCode = 301;
     return Response.redirect(destinationURL, statusCode);
 }
--- functions/orb.ts
+++ functions/orb.ts
@@ -1,9 +1,10 @@
 export async function onRequestPost({ env, request }) {
     const formData = await request.formData();
-    const params = {};
-    for (const entry of formData.entries()) {
-        params[entry[0]] = entry[1];
+    const alias = await formData.get('alias');
+    const url = await formData.get('url');
+    if (alias && alias.length > 0 && alias.length <= 128 && url && url.length > 0 && url.length <= 2048) {
+        await env.links.put(alias, url);
+        return new Response(`the worm summoned your link https://magi.lol/${alias}.\n`);
     }
-    const newLink = await env.links.put(params.alias, params.url);
-    return new Response(newLink);
+    return new Response("the orb rejected your request.\n");
 }
