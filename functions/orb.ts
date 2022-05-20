9b6eaced68fd44adaaf4e5f2f001275539a407f4
astrolince <astro@astrolince.com>
2022-05-19 21:04:17 -0300

    reject rewrites

--- functions/orb.ts
+++ functions/orb.ts
@@ -3,6 +3,10 @@ export async function onRequestPost({ env, request }) {
     const alias = await formData.get('alias');
     const url = await formData.get('url');
     if (alias && alias.length > 0 && alias.length <= 128 && url && url.length > 0 && url.length <= 2048) {
+        const checkIfExists = await env.links.get(alias, { cacheTtl: 86400 });
+        if (checkIfExists) {
+            return new Response("the orb rejected your request.\n");
+        }
         await env.links.put(alias, url);
         return new Response(`the worm summoned your link https://magi.lol/${alias}.\n`);
     }
