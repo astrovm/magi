d6265601c27e712025ff73e137001e78f116fb55
astrolince <astro@astrolince.com>
2022-05-20 03:13:42 -0300

    testing md5

--- functions/[alias].ts
+++ functions/[alias].ts
@@ -1,13 +1,17 @@
+import { hash } from "spark-md5";
+
 export const onRequestGet = async ({ env, params, request }): Promise<Response> => {
     const alias: string = params.alias;
     if (alias.includes('.')) {
         return env.ASSETS.fetch(request);
     }
-    if (alias.length > 256) {
+    if (alias.length > 4096) {
         return new Response(`the orb rejected your link.\n`);
     }
     const aliasEncoded: string = decodeURIComponent(alias).toLowerCase().replace(/\s/g, '-');
-    const destinationURL: string = await env.links.get(aliasEncoded, { cacheTtl: 86400 });
+    const aliasHash = hash(aliasEncoded);
+
+    const destinationURL: string = await env.links.get(aliasHash, { cacheTtl: 86400 });
     const statusCode: number = 301;
     return Response.redirect(destinationURL, statusCode);
 }
--- functions/orb.ts
+++ functions/orb.ts
@@ -1,3 +1,5 @@
+import { hash } from "spark-md5";
+
 const isAValidUrl = (input: string): boolean => {
     try {
         new URL(input);
@@ -19,22 +21,27 @@ export const onRequestPost = async ({ env, request }): Promise<Response> => {
     const formData: FormData = await request.formData();
     const alias: File | string = formData.get(`alias`);
     const url: File | string = formData.get(`url`);
+
     if (!alias || typeof alias !== 'string' || !url || typeof url !== 'string') {
         return new Response(`the orb rejected your request.\n`);
     }
+
     const aliasReplaceSpaces: string = alias.replace(/\s/g, '-');
-    if (alias.length < 4 || alias.length > 128 || alias.includes(`.`) || hasSpecialChars(aliasReplaceSpaces)) {
+    if (alias.length < 4 || alias.length > 2048 || alias.includes(`.`) || hasSpecialChars(aliasReplaceSpaces)) {
         return new Response(`the orb rejected your alias.\n`);
     }
-    if (url.length < 4 || url.length > 2048 || !isAValidUrl(url)) {
+    if (url.length > 2048 || !isAValidUrl(url)) {
         return new Response(`the orb rejected your invalid url.\n`);
     }
+
     const aliasLowerCase: string = aliasReplaceSpaces.toLowerCase();
-    const checkIfExists: KVNamespace["get"] = await env.links.get(aliasLowerCase, { cacheTtl: 86400 });
+    const aliasHash = hash(aliasLowerCase);
+
+    const checkIfExists: KVNamespace["get"] = await env.links.get(aliasHash, { cacheTtl: 86400 });
     if (checkIfExists) {
         return new Response(`the orb rejected your access to rewrite this link.\n`);
     }
-    await env.links.put(aliasLowerCase, url);
+    await env.links.put(aliasHash, url);
     return new Response(
         `the worm summoned your link https://magi.lol/${aliasReplaceSpaces}\n`
     );
