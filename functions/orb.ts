419f8e91d7a75074b64ef289e31de961b94f44f4
astrolince <astro@astrolince.com>
2022-05-19 21:39:48 -0300

    check valid url

--- functions/orb.ts
+++ functions/orb.ts
@@ -1,14 +1,36 @@
+function isAValidUrl(value: string): boolean {
+    try {
+        const url = new URL(value);
+        return true;
+    } catch (TypeError) {
+        return false;
+    }
+}
+
 export async function onRequestPost({ env, request }) {
     const formData = await request.formData();
-    const alias = await formData.get('alias');
-    const url = await formData.get('url');
-    if (alias && alias.length > 0 && alias.length <= 128 && url && url.length > 0 && url.length <= 2048) {
-        const checkIfExists = await env.links.get(alias, { cacheTtl: 86400 });
-        if (checkIfExists) {
-            return new Response("the orb rejected your request.\n");
-        }
-        await env.links.put(alias, url);
-        return new Response(`the worm summoned your link https://magi.lol/${alias}.\n`);
+    const alias = await formData.get(`alias`);
+    const url = await formData.get(`url`);
+    if (!isAValidUrl(url)) {
+        return new Response(`the orb rejected your invalid url.\n`);
     }
-    return new Response("the orb rejected your request.\n");
+    if (
+        !alias ||
+        alias.includes(`.`) ||
+        alias.length < 4 ||
+        alias.length > 128 ||
+        !url ||
+        url.length < 4 ||
+        url.length > 2048
+    ) {
+        return new Response(`the orb rejected your alias.\n`);
+    }
+    const checkIfExists = await env.links.get(alias, { cacheTtl: 86400 });
+    if (checkIfExists) {
+        return new Response(`the orb rejected your access to rewrite this link.\n`);
+    }
+    await env.links.put(alias, url);
+    return new Response(
+        `the worm summoned your link https://magi.lol/${alias}.\n`
+    );
 }
