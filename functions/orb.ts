92b3dcee3a87379593ad3c47a5f0c3edfe388288
astrolince <astro@astrolince.com>
2022-05-19 22:49:55 -0300

    move url conditions to url error

--- functions/orb.ts
+++ functions/orb.ts
@@ -1,8 +1,3 @@
-export async function onRequestGet({ env, request }): Promise<Response> {
-    const origin = new URL(request.url).origin;
-    return env.ASSETS.fetch(new Request(origin));
-}
-
 function isAValidUrl(value: string): boolean {
     try {
         const url = new URL(value);
@@ -16,19 +11,20 @@ export async function onRequestPost({ env, request }): Promise<Response> {
     const formData = await request.formData();
     const alias = await formData.get(`alias`);
     const url = await formData.get(`url`);
-    if (!isAValidUrl(url)) {
-        return new Response(`the orb rejected your invalid url.\n`);
-    }
     if (
         !alias ||
-        alias.includes(`.`) ||
         alias.length < 4 ||
         alias.length > 128 ||
-        !url ||
-        url.length < 4 ||
-        url.length > 2048
+        alias.includes(`.`)
+
     ) {
         return new Response(`the orb rejected your alias.\n`);
+    }
+    if (!url ||
+        url.length < 4 ||
+        url.length > 2048 ||
+        !isAValidUrl(url)) {
+        return new Response(`the orb rejected your invalid url.\n`);
     }
     const checkIfExists = await env.links.get(alias, { cacheTtl: 86400 });
     if (checkIfExists) {
