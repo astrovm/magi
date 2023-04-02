2b46786e2138c69f5d21408ae7b39c5987a6c4c1
astrolince <astro@astrolince.com>
2023-04-02 19:40:16 -0300

    improve orb

--- functions/orb.ts
+++ functions/orb.ts
@@ -1,17 +1,32 @@
 import Alias from '../modules/aliasClass';
 import Url from '../modules/urlClass';
 
-export const onRequestPost = async (
-  { env, request }: { env: any, request: Request },
-): Promise<Response> => {
-  const formFields: FormData = await request.formData();
-  const aliasField: File | string = formFields.get('alias');
-  const urlField: File | string = formFields.get('url');
+type RequestData = { env: any, request: Request };
 
-  if (!aliasField || typeof aliasField !== 'string' || !urlField || typeof urlField !== 'string') {
+function isString(value: any): value is string {
+  return typeof value === 'string';
+}
+
+async function validateInputs(formFields: FormData): Promise<[string, string] | null> {
+  const aliasField = formFields.get('alias');
+  const urlField = formFields.get('url');
+
+  if (isString(aliasField) && isString(urlField)) {
+    return [aliasField, urlField];
+  }
+
+  return null;
+}
+
+export const onRequestPost = async ({ env, request }: RequestData): Promise<Response> => {
+  const formFields: FormData = await request.formData();
+  const inputs = await validateInputs(formFields);
+
+  if (!inputs) {
     return new Response('the orb rejected your request.\n');
   }
 
+  const [aliasField, urlField] = inputs;
   const alias: Alias = new Alias(aliasField);
   alias.replaceSpacesWith('-');
   if (alias.lenghtIsGreaterThan(13312) || alias.includes('.') || alias.hasSpecialChars()) {
