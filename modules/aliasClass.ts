c3d690af86d056c4a15c3b8a99af44e415598150
astro <github.u3vwv@passfwd.com>
2025-10-21 20:44:57 -0300

    Handle invalid percent encodings during alias normalization

--- modules/aliasClass.ts
+++ modules/aliasClass.ts
@@ -27,8 +27,20 @@ export default class Alias {
     return hasSpecialChars(this.alias);
   }
 
-  decode(): void {
-    this.alias = decodeURIComponent(this.alias);
+  decode(): boolean {
+    const originalAlias = this.alias;
+
+    try {
+      this.alias = decodeURIComponent(this.alias);
+      return true;
+    } catch (error) {
+      if (error instanceof URIError) {
+        this.alias = originalAlias;
+        return false;
+      }
+
+      throw error;
+    }
   }
 
   normalize(): string {
